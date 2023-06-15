debbc757fb8176425241971c30da5b08bb1bda56
Agnes <129628079+agnesmva@users.noreply.github.com>
2023-06-15 19:39:46 -0300

    Add files via upload
    fiz alterações para quando o usuário inserir letras

--- atividade 07/calcular.js
+++ atividade 07/calcular.js
@@ -1,36 +1,59 @@
 alert("A sua página HTML5 está carregada! Vamos aprender a trabalhar com operadores de matemática básica")
 
-        function somar() {
-            var num1 = document.getElementById("numero1").value;
-            var num2 = document.getElementById("numero2").value;
-            var n1 = Number(num1);
-            var n2 = Number(num2);
-            var resultado = n1 + n2;
-            document.getElementById("resultado").textContent = "O resultado da soma é " + resultado;
-        }
+function somar() {
+    var num1 = document.getElementById("numero1").value;
+    var num2 = document.getElementById("numero2").value;
+    var n1 = Number(num1);
+    var n2 = Number(num2);
+    var resultado = n1 + n2;
+    document.getElementById("resultado").textContent = "O resultado da soma é " + resultado;
+    if (isNaN(resultado)){
+        document.getElementById("resultado").textContent = "Insira dois números para fazer a operação";
+    }
+    else {
+        document.getElementById("resultado").textContent = "O resultado da divisão é " + resultado;
+    }
+}
 
-        function subtrair() {
-            var num1 = document.getElementById("numero1").value;
-            var num2 = document.getElementById("numero2").value;
-            var n1 = Number(num1);
-            var n2 = Number(num2);
-            var resultado = n1 - n2;
-            document.getElementById("resultado").textContent = "O resultado da subtração é " + resultado;
-        }
-        function multiplicar(){
-            var num1 = document.getElementById("numero1").value;
-            var num2 = document.getElementById("numero2").value;
-            var n1 = Number(num1);
-            var n2 = Number(num2);
-            var resultado = n1 * n2;
-            document.getElementById("resultado").textContent = "O resultado da multiplicação é " + resultado;
-        }
-        function dividir(){
-            var num1 = document.getElementById("numero1").value;
-            var num2 = document.getElementById("numero2").value;
-            var n1 = Number(num1);
-            var n2 = Number(num2);
-            var resultado = n1 / n2;
-            document.getElementById("resultado").textContent = "O resultado da divisão é " + resultado;
-        }
-    
+function subtrair() {
+    var num1 = document.getElementById("numero1").value;
+    var num2 = document.getElementById("numero2").value;
+    var n1 = Number(num1);
+    var n2 = Number(num2);
+    var resultado = n1 - n2;
+    document.getElementById("resultado").textContent = "O resultado da subtração é " + resultado;
+    if (isNaN(resultado)){
+        document.getElementById("resultado").textContent = "Insira dois números para fazer a operação";
+    }
+    else {
+        document.getElementById("resultado").textContent = "O resultado da divisão é " + resultado;
+    }
+}
+function multiplicar(){
+    var num1 = document.getElementById("numero1").value;
+    var num2 = document.getElementById("numero2").value;
+    var n1 = Number(num1);
+    var n2 = Number(num2);
+    var resultado = n1 * n2;s
+    document.getElementById("resultado").textContent = "O resultado da multiplicação é " + resultado;
+    if (isNaN(resultado)){
+        document.getElementById("resultado").textContent = "Insira dois números para fazer a operação";
+    }
+    else {
+        document.getElementById("resultado").textContent = "O resultado da divisão é " + resultado;
+    }
+}
+function dividir(){
+    var num1 = document.getElementById("numero1").value;
+    var num2 = document.getElementById("numero2").value;
+    var n1 = Number(num1);
+    var n2 = Number(num2);
+    var resultado = n1 / n2;
+
+    if (isNaN(resultado)){
+        document.getElementById("resultado").textContent = "Insira dois números para fazer a operação";
+    }
+    else {
+        document.getElementById("resultado").textContent = "O resultado da divisão é " + resultado;
+    }
+}
